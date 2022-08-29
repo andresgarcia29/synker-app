fed63341d68f72a7522def9232183efcb5cc0981
Andres Garcia <jose.andres.gm29@gmail.com>
2022-08-28 20:07:55 -0500

    feat(init): A-19 Use env configuration in nuxtjs

--- components/Information.vue
+++ components/Information.vue
@@ -23,7 +23,7 @@ export default {
   name: 'information',
   methods: {
     getLoginUrl() {
-      return `${process.env.baseUrl || '0.0.0.0:8000'}/spotify/login`
+      return `${process.env.baseUrl}/spotify/login`
     },
   },
 }
